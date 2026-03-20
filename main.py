269774c71d4c6f6179cea15e71d402d4859f0c29
charminseon <k.emily0.0yz@gmail.com>
2026-03-20 15:38:37 +0900

    youtube Thumb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from langgraph!")
+﻿from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+
+class State(TypedDict):
+    video_file: str
+    audio_file: str
+    transcript: str
 
 
-if __name__ == "__main__":
-    main()
